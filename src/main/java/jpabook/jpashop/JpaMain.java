c2329dac41f147473282a16599adbccf6b803378
Hongsugyeong <olivia258@naver.com>
2023-12-25 17:36:03 +0900

    [Feat] 상속관계 매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -19,10 +20,11 @@ public class JpaMain {
         tx.begin();
 
         try{
-
-            Order order = new Order();
-            em.persist(order);
-            order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("홍수경");
+            
+            em.persist(book);
 
             tx.commit();
 
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,17 +3,11 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 
 @Entity
-public class OrderItem {
+public class OrderItem extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
-
-//    @Column(name = "ORDER_ID")
-//    private Long orderId;
-//
-//    @Column(name = "ITEM_ID")
-//    private Long itemId;
 
     @ManyToOne
     @JoinColumn(name = "ORDER_ID")
@@ -35,23 +29,6 @@ public class OrderItem {
     public void setId(Long id) {
         this.id = id;
     }
-
-//    public Long getOrderId() {
-//        return orderId;
-//    }
-//
-//    public void setOrderId(Long orderId) {
-//        this.orderId = orderId;
-//    }
-//
-//    public Long getItemId() {
-//        return itemId;
-//    }
-//
-//    public void setItemId(Long itemId) {
-//        this.itemId = itemId;
-//    }
-
 
     public Order getOrder() {
         return order;
